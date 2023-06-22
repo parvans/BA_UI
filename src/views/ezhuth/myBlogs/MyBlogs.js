9bdb94ab41e3852316c26cd8acf40ecc322a5434
parvans <parvansajeevan@gmail.com>
2023-06-22 17:05:28 +0530

    commend added for recognize

--- src/views/ezhuth/myBlogs/MyBlogs.js
+++ src/views/ezhuth/myBlogs/MyBlogs.js
@@ -160,6 +160,7 @@ export default function MyBlogs() {
             console.log(error);
         }
     }
+    //--------------------------------------------------------------------------------
     
 
     useEffect(() => {
