dfeb2c8049c86fa412d65b7e30b4bd2f71a81327
parvans <parvansajeevan@gmail.com>
2023-08-09 12:37:12 +0530

    ups

--- src/views/ezhuth/Chat/ChatArea.js
+++ src/views/ezhuth/Chat/ChatArea.js
@@ -1,20 +1,37 @@
-import ChatBox from 'components/miscellaneous/ChatBox'
-import MyChats from 'components/miscellaneous/MyChats'
-import { ChatState } from 'context/ChatProvider'
-import React from 'react'
-import { Card, CardBody, Col, Row } from 'reactstrap'
+import { Divider } from "@mui/material";
+import ChatBox from "components/miscellaneous/ChatBox";
+import MyChats from "components/miscellaneous/MyChats";
+import { ChatState } from "context/ChatProvider";
+import React from "react";
+import { Card, CardBody, Col, Row } from "reactstrap";
 
 export default function ChatArea() {
-    const {user}=ChatState()
-    const [fetchAgain,setFetchAgain]=React.useState(false)
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = React.useState(false);
 
   return (
     <div className="content">
-            <Row>
-                {user&& <MyChats/>}
-                {user&& <ChatBox/>}
-            </Row>
-           
+      <Row>
+        {/* {user&& <MyChats/>}
+                {user&& <ChatBox/>} */}
+
+        <Col md="12">
+          <Card>
+            <CardBody>
+              <Row>
+                <Col md="4">
+                  {/* <MyChats/> */}
+                  <h1>mychat</h1>
+                </Col>
+                <Col md="8">
+                  {/* <MyChats/> */}
+                  <h1>chatbox</h1>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     </div>
-  )
+  );
 }
